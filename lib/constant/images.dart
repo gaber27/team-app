de0f1164b33d05e3dd0aaa9eaaa7d6468f353005
gaber27 <gabertarikgaber27@gmail.com>
2024-04-24 21:01:36 +0200

    finish constant file by initial images and sizes classes

---
+++ lib/constant/images.dart
@@ -0,0 +1,7 @@
+// This class contain all images and animation used in app
+class Images {
+
+  Images._();
+
+  static const String splashAnimation = 'assets/animation/splashAnimated.json';
+}
---
+++ lib/constant/sizes.dart
@@ -0,0 +1,9 @@
+import 'package:sizer/sizer.dart';
+
+// This class contain all sizes used in app
+class Sizes {
+
+  Sizes._();
+
+  static double pagePadding = 12.h;
+}
