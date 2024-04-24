85e97cf148a08491cbfe93fcfec02f4fce059508
gaber27 <gabertarikgaber27@gmail.com>
2024-04-24 20:43:33 +0200

    add All Colors used in mobile app

---
+++ lib/constant/colors.dart
@@ -0,0 +1,9 @@
+import 'package:hexcolor/hexcolor.dart';
+
+// This File For All Colors Used In Mobile App
+var mainColor = HexColor('#FFFFFF');
+var secondColor = HexColor('#515BDE');
+var headingColor = HexColor('#000000');
+var textColor = HexColor('#6F6F71');
+var redColor = HexColor('#F14434');
+var greyColor = HexColor('#C5C5C5');
